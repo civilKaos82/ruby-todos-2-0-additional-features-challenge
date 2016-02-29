98c1f6e73854c16375e5b5183b1d65fb0f5b3f76
Torey Hickman <torey@toreyhickman.com>
2016-02-29 10:36:54 -0800

    ItemParser module only parses file to items

--- item_parser.rb
+++ item_parser.rb
@@ -1,13 +1,5 @@
-class ItemParser
-  class NoFilenameError < StandardError; end
-
-  attr_reader :filename
-
-  def initialize(filename)
-    @filename = filename || raise(NoFilenameError, "An item parser needs the filename where item data is stored.")
-  end
-
-  def parse
+module ItemParser
+  def self.parse(filename)
     File.readlines(filename).map { |file_row| item_from_file_data(file_row) }
   end
 
@@ -30,15 +22,15 @@ class ItemParser
     file << "#{item}\n"
   end
 
-  def complete_from_file_data(file_row)
+  def self.complete_from_file_data(file_row)
     file_row.match(COMPLETE_MARKER)
   end
 
-  def description_from_file_data(file_row)
+  def self.description_from_file_data(file_row)
     file_row.sub(/^\[.\] /, "").strip
   end
 
-  def item_from_file_data(file_row)
+  def self.item_from_file_data(file_row)
     complete = complete_from_file_data(file_row)
     description = description_from_file_data(file_row)
     Item.new(complete: complete, description: description)
--- spec/item_parser_spec.rb
+++ spec/item_parser_spec.rb
@@ -1,75 +1,67 @@
 require_relative "spec_helper"
 
 describe ItemParser do
-  let(:parser) { ItemParser.new(TEST_DATA_FILENAME) }
-
-  it "has a filename" do
-    expect(parser.filename).to eq TEST_DATA_FILENAME
-  end
-
-  it "requires a filename" do
-    expect { ItemParser.new(nil) }.to raise_error(ItemParser::NoFilenameError)
-  end
-
   describe "parsing items from a file" do
-    before do
+    before(:all) do
       open(TEST_DATA_FILENAME, "w") do |item_data_file|
         item_data_file << "[X] Walk the cat.\n"
         item_data_file << "[ ] Hit the gym.\n"
       end
     end
 
+    let(:parsed_items) { ItemParser.parse(TEST_DATA_FILENAME) }
+
     it "creates an object for each row in the file" do
-      expect(parser.parse.count).to eq 2
+      expect(parsed_items.count).to eq 2
     end
 
     it "creates completed items correctly" do
-      expect(parser.parse.first).to be_complete
+      expect(parsed_items.first).to be_complete
     end
 
     it "creates incomplete items correctly" do
-      expect(parser.parse.last).to be_incomplete
+      expect(parsed_items.last).to be_incomplete
     end
 
     it "assigns descriptions correctly" do
-      expect(parser.parse.first.description).to eq "Walk the cat."
-    end
-  end
-
-  describe "appending an item to the file" do
-    before(:all) do
-      open(TEST_DATA_FILENAME, "w") do |item_data_file|
-        item_data_file << "[X] Walk the cat.\n"
-      end
-    end
-
-    it "appends a line representing the item to the file" do
-      item = Item.new(description: "Learn something new.", complete: false)
-      parser.append(item)
-      expect(File.read(TEST_DATA_FILENAME)).to eq "[X] Walk the cat.\n[ ] Learn something new.\n"
-    end
-  end
-
-  describe "saving a collection of items" do
-    before(:all) do
-      open(TEST_DATA_FILENAME, "w") do |item_data_file|
-        item_data_file << "[X] Walk the cat.\n"
-      end
-    end
-
-    it "replaces the contents of the file with data representing the items" do
-      expect(File.read(TEST_DATA_FILENAME)).to eq "[X] Walk the cat.\n"
-
-      item_1 = Item.new(description: "Eat breakfast.", complete: true)
-      item_2 = Item.new(description: "Eat lunch.", complete: false)
-      items = [item_1, item_2]
-
-      parser.save(items)
-      expect(File.read(TEST_DATA_FILENAME)).to eq "[X] Eat breakfast.\n[ ] Eat lunch.\n"
+      expect(parsed_items.first.description).to eq "Walk the cat."
     end
   end
 
   after(:all) do
     File.truncate(TEST_DATA_FILENAME, 0)
   end
+
+  # describe "appending an item to the file" do
+  #   before(:all) do
+  #     open(TEST_DATA_FILENAME, "w") do |item_data_file|
+  #       item_data_file << "[X] Walk the cat.\n"
+  #     end
+  #   end
+
+  #   it "appends a line representing the item to the file" do
+  #     item = Item.new(description: "Learn something new.", complete: false)
+  #     parser.append(item)
+  #     expect(File.read(TEST_DATA_FILENAME)).to eq "[X] Walk the cat.\n[ ] Learn something new.\n"
+  #   end
+  # end
+
+  # describe "saving a collection of items" do
+  #   before(:all) do
+  #     open(TEST_DATA_FILENAME, "w") do |item_data_file|
+  #       item_data_file << "[X] Walk the cat.\n"
+  #     end
+  #   end
+
+  #   it "replaces the contents of the file with data representing the items" do
+  #     expect(File.read(TEST_DATA_FILENAME)).to eq "[X] Walk the cat.\n"
+
+  #     item_1 = Item.new(description: "Eat breakfast.", complete: true)
+  #     item_2 = Item.new(description: "Eat lunch.", complete: false)
+  #     items = [item_1, item_2]
+
+  #     parser.save(items)
+  #     expect(File.read(TEST_DATA_FILENAME)).to eq "[X] Eat breakfast.\n[ ] Eat lunch.\n"
+  #   end
+  # end
 end
