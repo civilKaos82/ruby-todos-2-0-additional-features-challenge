e477c0fd138e05ae345ab7758f270dac626e9d1d
Torey Hickman <torey@toreyhickman.com>
2016-02-29 11:25:44 -0800

    ListView not dependent on Item#to_s

--- list_view.rb
+++ list_view.rb
@@ -4,9 +4,21 @@ class ListView
   end
 
   def to_s
-    list.map(&:to_s).join("\n")
+    list.map { |item| formatted_item(item) }.join("\n")
   end
 
   private
+  ITEM_COMPLETE_MARKER = "[X]"
+  ITEM_INCOMPLETE_MARKER = "[ ]"
+
   attr_reader :list
+
+  def formatted_item(item)
+    "#{marker_for(item)} #{item.description}"
+  end
+
+
+  def marker_for(item)
+    item.complete? ? ITEM_COMPLETE_MARKER : ITEM_INCOMPLETE_MARKER
+  end
 end
